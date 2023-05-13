595dfeb104fa476514c2aa5e52cce9cc68db5d23
Sitharakp01 <108386147+Sitharakp01@users.noreply.github.com>
2023-05-13 19:47:22 +0530

    Added new files

---
+++ script.js
@@ -0,0 +1,10 @@
+function showKnowMore(card) {
+    const knowMore = card.querySelector('.know-more');
+    knowMore.style.visibility = 'visible';
+  }
+  
+  function hideKnowMore(card) {
+    const knowMore = card.querySelector('.know-more');
+    knowMore.style.visibility = 'hidden';
+  }
+  
